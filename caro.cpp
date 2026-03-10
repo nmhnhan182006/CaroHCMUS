75e55ac9044473feeccb5993ca10ecfd2929fdc9
nhanp <nhanp1806@gmail.com>
2026-03-10 16:29:45 +0700

    Add project files.

---
+++ caro.cpp
@@ -0,0 +1,33 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <string>
+using namespace std;
+
+const int WIDTH = 1280;
+const int HEIGHT = 720;
+const int ROWS = 15;
+const int COLS = 15;
+const float CELL = 40.f;
+
+sf::RenderWindow window(sf::VideoMode({ WIDTH, HEIGHT }), "Caro Game");
+
+// màn hình
+int currentScreen = 0;
+// 0 = menu, 1 = playing, 2 = settings, 3 = exit
+
+// menu
+vector<string> menuItems = { "New Game", "Settings", "Exit" };
+int selectedMenu = 0;
+
+// bàn cờ
+int board[ROWS][COLS];
+int currentPlayer = 1; // 1 = X, 2 = O
+
+// asset
+sf::Font font;
+sf::Texture menuBgTexture;
+sf::Sprite menuBgSprite;
+
+sf::Texture xTexture;
+sf::Texture oTexture;
